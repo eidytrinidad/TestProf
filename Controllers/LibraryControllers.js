d9525531985ead26c598e2505b87d104acbecaa6
Eidy trinidad <eidyjtrinidadferreras@hotmail.com>
2021-07-02 17:11:14 -0400

    Publish

--- Controllers/LibraryControllers.js
+++ Controllers/LibraryControllers.js
@@ -1,83 +1,75 @@
-const Library= require("../Models/LibraryModel")
-
+const Library = require("../Models/LibraryModel");
 
 //Get all books
-async function getBooks(req,res) {
-    
-    try {
-        const books=await Library.findAllBooks()
-        res.writeHead(200,{"Content-Type":"application/json"})
+async function getBooks(req, res) {
+  try {
+    const books = await Library.findAllBooks();
+    res.writeHead(200, { "Content-Type": "application/json" });
 
-        res.end(JSON.stringify(books))
-    } catch (error) {
-        console.log(error)
-    }
+    res.end(JSON.stringify(books));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Get a single book
-async function getBook(req,res,id) {
-    
-    try {
-        const book=await Library.findBookById(id)
-        
-        if (!book) {
-            res.writeHead(404,{"Content-Type":"application/json"})
-            res.end(JSON.stringify({Message:'Book Not Found'}))
-        } else {
-            res.writeHead(200,{"Content-Type":"application/json"})
-            res.end(JSON.stringify(book))
-           
-        }
+async function getBook(req, res, id) {
+  try {
+    const book = await Library.findBookById(id);
 
-     
-    } catch (error) {
-        console.log(error)
+    if (!book) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ Message: "Book Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(book));
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Get book pages
-async function getBookPages(req,res,id) {
-    
-    try {
-        const pages=await Library.findBookPages(id)
-      console.log(pages)
+async function getBookPages(req, res, id) {
+  try {
+    const pages = await Library.findBookPages(id);
 
-        if (!pages) {
-            res.writeHead(404,{"Content-Type":"application/json"})
-            res.end(JSON.stringify({Message:'Pages Not Found'}))
-        } else {
-           
-            res.writeHead(200,{"Content-Type":"application/json"})
-            res.end(JSON.stringify(pages))
-           
-        }
-     
-    } catch (error) {
-        console.log(error)
+
+    if (!pages) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ Message: "Pages Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(pages));
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Get bookpage By page
-async function getBookPagesByPage(req,res,id,pageNumber) {
-    
-    try {
-        const page=await Library.findBookPagesByPage(id,pageNumber)
+async function getBookPagesByPage(req, res, id, pageNumber, format) {
+  try {
+    const page = await Library.findBookPagesByPage(id, pageNumber);
 
-
-        if (!page) {
-            res.writeHead(404,{"Content-Type":"application/json"})
-            res.end(JSON.stringify({Message:'Page Not Found'}))
-        } else {
-           
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.end(`<p>${page}</p>`)
-           
-        }
-
-     
-    } catch (error) {
-        console.log(error)
+    if (!page) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ Message: "Page Not Found" }));
+    } else {
+      if (format === "html") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(`<p>${page}</p>`);
+      } else if (format === "plain") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(page);
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ Message: "Formato no permitido" }));
+      }
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-module.exports={getBooks,getBook,getBookPages,getBookPagesByPage}
+module.exports = { getBooks, getBook, getBookPages, getBookPagesByPage };
--- app.js
+++ app.js
@@ -2,32 +2,49 @@ const http = require("http");
 
 const PORT = process.env.port || 4000;
 
+
+
 const { getBooks, getBook,getBookPages,getBookPagesByPage } = require("./Controllers/LibraryControllers");
 
 const server = http.createServer((req, res) => {
     
 const {url , method} = req
+
+
 //Route All Books
   if (url === "/api/books" && method === "GET") {
 
     getBooks(req, res);
 
+
   } 
 
-//Route Page by Number
-  else if (url.match(/\/api\/books\/([0-9]+)\/pages\/([0-9]+)/) && method === "GET") {
+//Route Page by Number and text/html
+  else if (url.match(/\/api\/books\/([0-9]+)\/pages\/([0-9]+)\/html/) && method === "GET") {
 
     const id = url.split("/")[3];
     const pageNumber = url.split("/")[5];
-    getBookPagesByPage(req, res, id,pageNumber);
-
+    const format=url.split("/")[6];
+    getBookPagesByPage(req, res, id,pageNumber,format);
+   
   } 
+
+  //Route Page by Number and format text/plain
+  else if (url.match(/\/api\/books\/([0-9]+)\/pages\/([0-9]+)\/plain/) && method === "GET") {
+
+    const id = url.split("/")[3];
+    const pageNumber = url.split("/")[5];
+    const format=url.split("/")[6];
+    getBookPagesByPage(req, res, id,pageNumber,format);
+   
+  } 
+
 
   //Route All Pages
   else if (url.match(/\/api\/books\/([0-9]+)\/pages/) && method === "GET") {
 
     const id = url.split("/")[3];
-    console.log(id)
+
     getBookPages(req, res, id);
 
   }
